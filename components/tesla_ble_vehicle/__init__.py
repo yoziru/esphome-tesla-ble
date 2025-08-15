56487210d99e2bcc8b2e682706dd128a42812a11
Yasir Ekinci <ekinci.yasir@gmail.com>
2025-08-16 01:50:47 +0200

    Enhance Tesla BLE vehicle component by adding new configuration options for charge polling interval and awake polling interval, and introduce additional sensors for battery level, charging state, charger power, and charge rate.

--- components/tesla_ble_vehicle/__init__.py
+++ components/tesla_ble_vehicle/__init__.py
@@ -1,7 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import ble_client, binary_sensor
-from esphome.const import CONF_ID
+from esphome.components import ble_client, binary_sensor, sensor, text_sensor
+from esphome.const import (
+    CONF_ID,
+    DEVICE_CLASS_BATTERY,
+    DEVICE_CLASS_DOOR,
+    DEVICE_CLASS_LOCK,
+    DEVICE_CLASS_OCCUPANCY,
+    DEVICE_CLASS_POWER,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_PERCENT,
+    UNIT_KILOWATT,
+)
 
 CODEOWNERS = ["@yoziru"]
 DEPENDENCIES = ["ble_client"]
@@ -11,12 +21,18 @@ TeslaBLEVehicle = tesla_ble_vehicle_ns.class_(
     "TeslaBLEVehicle", cg.PollingComponent, ble_client.BLEClientNode
 )
 
-AUTO_LOAD = ["binary_sensor"]
+AUTO_LOAD = ["binary_sensor", "sensor", "text_sensor"]
 CONF_VIN = "vin"
 CONF_IS_ASLEEP = "is_asleep"
 CONF_IS_UNLOCKED = "is_unlocked"
 CONF_IS_USER_PRESENT = "is_user_present"
 CONF_IS_CHARGE_FLAP_OPEN = "is_charge_flap_open"
+CONF_CHARGE_POLLING_INTERVAL = "charge_polling_interval"
+CONF_AWAKE_POLLING_INTERVAL = "awake_polling_interval"
+CONF_BATTERY_LEVEL = "battery_level"
+CONF_CHARGING_STATE = "charging_state"
+CONF_CHARGER_POWER = "charger_power"
+CONF_CHARGE_RATE = "charge_rate"
 
 CONFIG_SCHEMA = (
     cv.Schema(
@@ -28,13 +44,43 @@ CONFIG_SCHEMA = (
                 icon="mdi:sleep"
             ).extend(),
             cv.Optional(CONF_IS_UNLOCKED): binary_sensor.binary_sensor_schema(
-                device_class=binary_sensor.DEVICE_CLASS_LOCK,
+                device_class=DEVICE_CLASS_LOCK,
             ).extend(),
             cv.Optional(CONF_IS_USER_PRESENT): binary_sensor.binary_sensor_schema(
-                icon="mdi:account-check", device_class=binary_sensor.DEVICE_CLASS_OCCUPANCY
+                icon="mdi:account-check",
+                device_class=DEVICE_CLASS_OCCUPANCY,
             ).extend(),
             cv.Optional(CONF_IS_CHARGE_FLAP_OPEN): binary_sensor.binary_sensor_schema(
-                icon="mdi:ev-plug-tesla", device_class=binary_sensor.DEVICE_CLASS_DOOR
+                icon="mdi:ev-plug-tesla", device_class=DEVICE_CLASS_DOOR
+            ).extend(),
+            cv.Optional(
+                CONF_CHARGE_POLLING_INTERVAL, default="10s"
+            ): cv.positive_time_period_milliseconds,
+            cv.Optional(
+                CONF_AWAKE_POLLING_INTERVAL, default="5min"
+            ): cv.positive_time_period_milliseconds,
+            cv.Optional(CONF_BATTERY_LEVEL): sensor.sensor_schema(
+                unit_of_measurement=UNIT_PERCENT,
+                accuracy_decimals=0,
+                device_class=DEVICE_CLASS_BATTERY,
+                state_class=STATE_CLASS_MEASUREMENT,
+                icon="mdi:battery",
+            ).extend(),
+            cv.Optional(CONF_CHARGING_STATE): text_sensor.text_sensor_schema(
+                icon="mdi:ev-station"
+            ).extend(),
+            cv.Optional(CONF_CHARGER_POWER): sensor.sensor_schema(
+                unit_of_measurement=UNIT_KILOWATT,
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_POWER,
+                state_class=STATE_CLASS_MEASUREMENT,
+                icon="mdi:lightning-bolt",
+            ).extend(),
+            cv.Optional(CONF_CHARGE_RATE): sensor.sensor_schema(
+                unit_of_measurement="mph",
+                accuracy_decimals=0,
+                state_class=STATE_CLASS_MEASUREMENT,
+                icon="mdi:speedometer",
             ).extend(),
         }
     )
@@ -50,6 +96,8 @@ async def to_code(config):
     await ble_client.register_ble_node(var, config)
 
     cg.add(var.set_vin(config[CONF_VIN]))
+    cg.add(var.set_charge_polling_interval(config[CONF_CHARGE_POLLING_INTERVAL]))
+    cg.add(var.set_awake_polling_interval(config[CONF_AWAKE_POLLING_INTERVAL]))
 
     if CONF_IS_ASLEEP in config:
         conf = config[CONF_IS_ASLEEP]
@@ -70,3 +118,23 @@ async def to_code(config):
         conf = config[CONF_IS_CHARGE_FLAP_OPEN]
         bs = await binary_sensor.new_binary_sensor(conf)
         cg.add(var.set_binary_sensor_is_charge_flap_open(bs))
+
+    if CONF_BATTERY_LEVEL in config:
+        conf = config[CONF_BATTERY_LEVEL]
+        s = await sensor.new_sensor(conf)
+        cg.add(var.set_sensor_battery_level(s))
+
+    if CONF_CHARGING_STATE in config:
+        conf = config[CONF_CHARGING_STATE]
+        s = await text_sensor.new_text_sensor(conf)
+        cg.add(var.set_text_sensor_charging_state(s))
+
+    if CONF_CHARGER_POWER in config:
+        conf = config[CONF_CHARGER_POWER]
+        s = await sensor.new_sensor(conf)
+        cg.add(var.set_sensor_charger_power(s))
+
+    if CONF_CHARGE_RATE in config:
+        conf = config[CONF_CHARGE_RATE]
+        s = await sensor.new_sensor(conf)
+        cg.add(var.set_sensor_charge_rate(s))
--- components/tesla_ble_vehicle/tesla_ble_vehicle.h
+++ components/tesla_ble_vehicle/tesla_ble_vehicle.h
@@ -3,222 +3,288 @@
 #include <algorithm>
 #include <cstring>
 #include <iterator>
-#include <vector>
 #include <queue>
+#include <vector>
 
 #include <esp_gattc_api.h>
 #include <esphome/components/binary_sensor/binary_sensor.h>
 #include <esphome/components/ble_client/ble_client.h>
 #include <esphome/components/esp32_ble_tracker/esp32_ble_tracker.h>
 #include <esphome/components/sensor/sensor.h>
+#include <esphome/components/text_sensor/text_sensor.h>
 #include <esphome/core/component.h>
 #include <esphome/core/log.h>
 
+#include <errors.h>
 #include <universal_message.pb.h>
 #include <vcsec.pb.h>
-#include <errors.h>
 
-namespace TeslaBLE
-{
-    class Client;
+namespace TeslaBLE {
+class Client;
 }
 
-typedef enum BLE_CarServer_VehicleAction_E
-{
-    SET_CHARGING_SWITCH,
-    SET_CHARGING_AMPS,
-    SET_CHARGING_LIMIT
+typedef enum BLE_CarServer_VehicleAction_E {
+  SET_CHARGING_SWITCH,
+  SET_CHARGING_AMPS,
+  SET_CHARGING_LIMIT
 } BLE_CarServer_VehicleAction;
 
-namespace esphome
-{
+namespace esphome {
 
-    namespace tesla_ble_vehicle
-    {
-        namespace espbt = esphome::esp32_ble_tracker;
+namespace tesla_ble_vehicle {
+namespace espbt = esphome::esp32_ble_tracker;
 
-        static const char *const TAG = "tesla_ble_vehicle";
-        static const char *nvs_key_infotainment = "tk_infotainment";
-        static const char *nvs_key_vcsec = "tk_vcsec";
+static const char *const TAG = "tesla_ble_vehicle";
+static const char *nvs_key_infotainment = "tk_infotainment";
+static const char *nvs_key_vcsec = "tk_vcsec";
 
-        static const char *const SERVICE_UUID = "00000211-b2d1-43f0-9b88-960cebf8b91e";
-        static const char *const READ_UUID = "00000213-b2d1-43f0-9b88-960cebf8b91e";
-        static const char *const WRITE_UUID = "00000212-b2d1-43f0-9b88-960cebf8b91e";
+static const char *const SERVICE_UUID = "00000211-b2d1-43f0-9b88-960cebf8b91e";
+static const char *const READ_UUID = "00000213-b2d1-43f0-9b88-960cebf8b91e";
+static const char *const WRITE_UUID = "00000212-b2d1-43f0-9b88-960cebf8b91e";
 
-        static const int PRIVATE_KEY_SIZE = 228;
-        static const int PUBLIC_KEY_SIZE = 65;
-        static const int MAX_BLE_MESSAGE_SIZE = 1024; // Max size of a BLE message
-        static const int RX_TIMEOUT = 1 * 1000;       // Timeout interval between receiving chunks of a message (1s)
-        static const int MAX_LATENCY = 4 * 1000;      // Max allowed error when syncing vehicle clock (4s)
-        static const int BLOCK_LENGTH = 20;           // BLE MTU is 23 bytes, so we need to split the message into chunks (20 bytes as in vehicle_command)
-        static const int MAX_RETRIES = 5;             // Max number of retries for a command
-        static const int COMMAND_TIMEOUT = 30 * 1000; // Overall timeout for a command (30s)
+static const int PRIVATE_KEY_SIZE = 228;
+static const int PUBLIC_KEY_SIZE = 65;
+static const int MAX_BLE_MESSAGE_SIZE = 1024; // Max size of a BLE message
+static const int RX_TIMEOUT =
+    1 * 1000; // Timeout interval between receiving chunks of a message (1s)
+static const int MAX_LATENCY =
+    4 * 1000; // Max allowed error when syncing vehicle clock (4s)
+static const int BLOCK_LENGTH =
+    20; // BLE MTU is 23 bytes, so we need to split the message into chunks (20
+        // bytes as in vehicle_command)
+static const int MAX_RETRIES = 5; // Max number of retries for a command
+static const int COMMAND_TIMEOUT =
+    30 * 1000; // Overall timeout for a command (30s)
 
-        enum class BLECommandState
-        {
-            IDLE,
-            WAITING_FOR_VCSEC_AUTH,
-            WAITING_FOR_VCSEC_AUTH_RESPONSE,
-            WAITING_FOR_INFOTAINMENT_AUTH,
-            WAITING_FOR_INFOTAINMENT_AUTH_RESPONSE,
-            WAITING_FOR_WAKE,
-            WAITING_FOR_WAKE_RESPONSE,
-            READY,
-            WAITING_FOR_RESPONSE,
-        };
+enum class BLECommandState {
+  IDLE,
+  WAITING_FOR_VCSEC_AUTH,
+  WAITING_FOR_VCSEC_AUTH_RESPONSE,
+  WAITING_FOR_INFOTAINMENT_AUTH,
+  WAITING_FOR_INFOTAINMENT_AUTH_RESPONSE,
+  WAITING_FOR_WAKE,
+  WAITING_FOR_WAKE_RESPONSE,
+  READY,
+  WAITING_FOR_RESPONSE,
+};
 
-        struct BLECommand
-        {
-            UniversalMessage_Domain domain;
-            std::function<int()> execute;
-            std::string execute_name;
-            BLECommandState state;
-            uint32_t started_at = millis();
-            uint32_t last_tx_at = 0;
-            uint8_t retry_count = 0;
+struct BLECommand {
+  UniversalMessage_Domain domain;
+  std::function<int()> execute;
+  std::string execute_name;
+  BLECommandState state;
+  uint32_t started_at = millis();
+  uint32_t last_tx_at = 0;
+  uint8_t retry_count = 0;
 
-            BLECommand(UniversalMessage_Domain d, std::function<int()> e, std::string n = "")
-                : domain(d), execute(e), execute_name(n), state(BLECommandState::IDLE) {}
-        };
+  BLECommand(UniversalMessage_Domain d, std::function<int()> e,
+             std::string n = "")
+      : domain(d), execute(e), execute_name(n), state(BLECommandState::IDLE) {}
+};
 
-        struct BLETXChunk
-        {
-            std::vector<unsigned char> data;
-            esp_gatt_write_type_t write_type;
-            esp_gatt_auth_req_t auth_req;
-            uint32_t sent_at = millis();
-            uint8_t retry_count = 0;
+struct BLETXChunk {
+  std::vector<unsigned char> data;
+  esp_gatt_write_type_t write_type;
+  esp_gatt_auth_req_t auth_req;
+  uint32_t sent_at = millis();
+  uint8_t retry_count = 0;
 
-            BLETXChunk(std::vector<unsigned char> d, esp_gatt_write_type_t wt, esp_gatt_auth_req_t ar)
-                : data(d), write_type(wt), auth_req(ar) {}
-        };
+  BLETXChunk(std::vector<unsigned char> d, esp_gatt_write_type_t wt,
+             esp_gatt_auth_req_t ar)
+      : data(d), write_type(wt), auth_req(ar) {}
+};
 
-        struct BLERXChunk
-        {
-            std::vector<unsigned char> buffer;
-            uint32_t received_at = millis();
+struct BLERXChunk {
+  std::vector<unsigned char> buffer;
+  uint32_t received_at = millis();
 
-            BLERXChunk(std::vector<unsigned char> b)
-                : buffer(b) {}
-        };
+  BLERXChunk(std::vector<unsigned char> b) : buffer(b) {}
+};
 
-        struct BLEResponse
-        {
-            // universal message
-            UniversalMessage_RoutableMessage message;
-            uint32_t received_at = millis();
+struct BLEResponse {
+  // universal message
+  UniversalMessage_RoutableMessage message;
+  uint32_t received_at = millis();
 
-            BLEResponse(UniversalMessage_RoutableMessage m)
-                : message(m) {}
-        };
+  BLEResponse(UniversalMessage_RoutableMessage m) : message(m) {}
+};
 
-        class TeslaBLEVehicle : public PollingComponent,
-                                public ble_client::BLEClientNode
-        {
-        public:
-            TeslaBLEVehicle();
-            void setup() override;
-            void loop() override;
-            void update() override;
-            void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
-                                     esp_ble_gattc_cb_param_t *param) override;
-            void dump_config() override;
-            void set_vin(const char *vin);
-            void process_command_queue();
-            void process_response_queue();
-            void process_ble_read_queue();
-            void process_ble_write_queue();
-            void invalidateSession(UniversalMessage_Domain domain);
+class TeslaBLEVehicle : public PollingComponent,
+                        public ble_client::BLEClientNode {
+public:
+  TeslaBLEVehicle();
+  void setup() override;
+  void loop() override;
+  void update() override;
+  void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
+                           esp_ble_gattc_cb_param_t *param) override;
+  void dump_config() override;
+  void set_vin(const char *vin);
+  void process_command_queue();
+  void process_response_queue();
+  void process_ble_read_queue();
+  void process_ble_write_queue();
+  void invalidateSession(UniversalMessage_Domain domain);
 
-            void regenerateKey();
-            int startPair(void);
-            int nvs_save_session_info(const Signatures_SessionInfo &session_info, const UniversalMessage_Domain domain);
-            int nvs_load_session_info(Signatures_SessionInfo *session_info, const UniversalMessage_Domain domain);
-            int nvs_initialize_private_key();
+  void regenerateKey();
+  int startPair(void);
+  int nvs_save_session_info(const Signatures_SessionInfo &session_info,
+                            const UniversalMessage_Domain domain);
+  int nvs_load_session_info(Signatures_SessionInfo *session_info,
+                            const UniversalMessage_Domain domain);
+  int nvs_initialize_private_key();
 
-            int handleSessionInfoUpdate(UniversalMessage_RoutableMessage message, UniversalMessage_Domain domain);
-            int handleVCSECVehicleStatus(VCSEC_VehicleStatus vehicleStatus);
+  int handleSessionInfoUpdate(UniversalMessage_RoutableMessage message,
+                              UniversalMessage_Domain domain);
+  int handleVCSECVehicleStatus(VCSEC_VehicleStatus vehicleStatus);
+  int handleCarServerVehicleData(const CarServer_VehicleData &vehicleData);
 
-            int wakeVehicle(void);
-            int sendVCSECActionMessage(VCSEC_RKEAction_E action);
-            int sendCarServerGetVehicleData(void);
-            int sendCarServerVehicleActionMessage(BLE_CarServer_VehicleAction action, int param);
+  int wakeVehicle(void);
+  int sendVCSECActionMessage(VCSEC_RKEAction_E action);
+  int sendCarServerGetVehicleData(void);
+  int sendCarServerVehicleActionMessage(BLE_CarServer_VehicleAction action,
+                                        int param);
 
-            int sendSessionInfoRequest(UniversalMessage_Domain domain);
-            int sendVCSECInformationRequest(void);
-            void enqueueVCSECInformationRequest(bool force = false);
+  int sendSessionInfoRequest(UniversalMessage_Domain domain);
+  int sendVCSECInformationRequest(void);
+  void enqueueVCSECInformationRequest(bool force = false);
 
-            int writeBLE(const unsigned char *message_buffer, size_t message_length,
-                         esp_gatt_write_type_t write_type, esp_gatt_auth_req_t auth_req);
+  int writeBLE(const unsigned char *message_buffer, size_t message_length,
+               esp_gatt_write_type_t write_type, esp_gatt_auth_req_t auth_req);
 
-            // sensors
-            // Sleep state (vehicleSleepStatus)
-            void set_binary_sensor_is_asleep(binary_sensor::BinarySensor *s) { isAsleepSensor = static_cast<binary_sensor::BinarySensor *>(s); }
-            void updateIsAsleep(bool asleep)
-            {
-                isAsleepSensor->publish_state(asleep);
-            }
-            // Door lock (vehicleLockState)
-            void set_binary_sensor_is_unlocked(binary_sensor::BinarySensor *s) { isUnlockedSensor = static_cast<binary_sensor::BinarySensor *>(s); }
-            void updateisUnlocked(bool locked)
-            {
-                isUnlockedSensor->publish_state(locked);
-            }
-            // User presence (userPresence)
-            void set_binary_sensor_is_user_present(binary_sensor::BinarySensor *s) { isUserPresentSensor = static_cast<binary_sensor::BinarySensor *>(s); }
-            void updateIsUserPresent(bool present)
-            {
-                isUserPresentSensor->publish_state(present);
-            }
-            // Charge flap (chargeFlapStatus)
-            void set_binary_sensor_is_charge_flap_open(binary_sensor::BinarySensor *s) { isChargeFlapOpenSensor = static_cast<binary_sensor::BinarySensor *>(s); }
-            void updateIsChargeFlapOpen(bool open)
-            {
-                isChargeFlapOpenSensor->publish_state(open);
-            }
-            void setChargeFlapHasState(bool has_state)
-            {
-                isChargeFlapOpenSensor->set_has_state(has_state);
-            }
+  // sensors
+  // Sleep state (vehicleSleepStatus)
+  void set_binary_sensor_is_asleep(binary_sensor::BinarySensor *s) {
+    this->isAsleepSensor = static_cast<binary_sensor::BinarySensor *>(s);
+  }
+  void updateIsAsleep(bool asleep) {
+    this->isAsleepSensor->publish_state(asleep);
+  }
+  // Door lock (vehicleLockState)
+  void set_binary_sensor_is_unlocked(binary_sensor::BinarySensor *s) {
+    this->isUnlockedSensor = static_cast<binary_sensor::BinarySensor *>(s);
+  }
+  void updateisUnlocked(bool locked) {
+    this->isUnlockedSensor->publish_state(locked);
+  }
+  // User presence (userPresence)
+  void set_binary_sensor_is_user_present(binary_sensor::BinarySensor *s) {
+    this->isUserPresentSensor = static_cast<binary_sensor::BinarySensor *>(s);
+  }
+  void updateIsUserPresent(bool present) {
+    this->isUserPresentSensor->publish_state(present);
+  }
+  // Charge flap (chargeFlapStatus)
+  void set_binary_sensor_is_charge_flap_open(binary_sensor::BinarySensor *s) {
+    this->isChargeFlapOpenSensor =
+        static_cast<binary_sensor::BinarySensor *>(s);
+  }
+  void updateIsChargeFlapOpen(bool open) {
+    this->isChargeFlapOpenSensor->publish_state(open);
+  }
+  void setChargeFlapHasState(bool has_state) {
+    this->isChargeFlapOpenSensor->set_has_state(has_state);
+  }
 
-            // set sensors to unknown (e.g. when vehicle is disconnected)
-            void setSensors(bool has_state)
-            {
-                isAsleepSensor->set_has_state(has_state);
-                isUnlockedSensor->set_has_state(has_state);
-                isUserPresentSensor->set_has_state(has_state);
-            }
+  // Charge state sensors
+  void set_sensor_battery_level(sensor::Sensor *s) {
+    this->batteryLevelSensor = s;
+  }
+  void set_text_sensor_charging_state(text_sensor::TextSensor *s) {
+    this->chargingStateSensor = s;
+  }
+  void set_sensor_charger_power(sensor::Sensor *s) {
+    this->chargerPowerSensor = s;
+  }
+  void set_sensor_charge_rate(sensor::Sensor *s) { this->chargeRateSensor = s; }
+  void set_charge_polling_interval(uint32_t interval) {
+    this->charge_polling_interval_ = interval;
+  }
+  void set_awake_polling_interval(uint32_t interval) {
+    this->awake_polling_interval_ = interval;
+  }
 
-        protected:
-            std::queue<BLERXChunk> ble_read_queue_;
-            std::queue<BLEResponse> response_queue_;
-            std::queue<BLETXChunk> ble_write_queue_;
-            std::queue<BLECommand> command_queue_;
+  void updateBatteryLevel(float level) {
+    if (this->batteryLevelSensor != nullptr) {
+      this->batteryLevelSensor->publish_state(level);
+    }
+  }
+  void updateChargingState(const std::string &state) {
+    if (this->chargingStateSensor != nullptr) {
+      this->chargingStateSensor->publish_state(state);
+    }
+  }
+  void updateChargerPower(float power) {
+    if (this->chargerPowerSensor != nullptr) {
+      this->chargerPowerSensor->publish_state(power);
+    }
+  }
+  void updateChargeRate(float rate) {
+    if (this->chargeRateSensor != nullptr) {
+      this->chargeRateSensor->publish_state(rate);
+    }
+  }
 
-            TeslaBLE::Client *tesla_ble_client_;
-            uint32_t storage_handle_;
-            uint16_t handle_;
-            uint16_t read_handle_{0};
-            uint16_t write_handle_{0};
+  // set sensors to unknown (e.g. when vehicle is disconnected)
+  void setSensors(bool has_state) {
+    this->isAsleepSensor->set_has_state(has_state);
+    this->isUnlockedSensor->set_has_state(has_state);
+    this->isUserPresentSensor->set_has_state(has_state);
+    if (this->batteryLevelSensor != nullptr)
+      this->batteryLevelSensor->set_has_state(has_state);
+    if (this->chargingStateSensor != nullptr)
+      this->chargingStateSensor->set_has_state(has_state);
+    if (this->chargerPowerSensor != nullptr)
+      this->chargerPowerSensor->set_has_state(has_state);
+    if (this->chargeRateSensor != nullptr)
+      this->chargeRateSensor->set_has_state(has_state);
+  }
 
-            espbt::ESPBTUUID service_uuid_;
-            espbt::ESPBTUUID read_uuid_;
-            espbt::ESPBTUUID write_uuid_;
+protected:
+  std::queue<BLERXChunk> ble_read_queue_;
+  std::queue<BLEResponse> response_queue_;
+  std::queue<BLETXChunk> ble_write_queue_;
+  std::queue<BLECommand> command_queue_;
 
-            // sensors
-            binary_sensor::BinarySensor *isAsleepSensor;
-            binary_sensor::BinarySensor *isUnlockedSensor;
-            binary_sensor::BinarySensor *isUserPresentSensor;
-            binary_sensor::BinarySensor *isChargeFlapOpenSensor;
+  TeslaBLE::Client *tesla_ble_client_;
+  uint32_t storage_handle_;
+  uint16_t handle_;
+  uint16_t read_handle_{0};
+  uint16_t write_handle_{0};
 
-            std::vector<unsigned char> ble_read_buffer_;
+  espbt::ESPBTUUID service_uuid_;
+  espbt::ESPBTUUID read_uuid_;
+  espbt::ESPBTUUID write_uuid_;
 
-            void initializeFlash();
-            void openNVSHandle();
-            void initializePrivateKey();
-            void loadSessionInfo();
-            void loadDomainSessionInfo(UniversalMessage_Domain domain);
-        };
+  // sensors
+  binary_sensor::BinarySensor *isAsleepSensor;
+  binary_sensor::BinarySensor *isUnlockedSensor;
+  binary_sensor::BinarySensor *isUserPresentSensor;
+  binary_sensor::BinarySensor *isChargeFlapOpenSensor;
 
-    } // namespace tesla_ble_vehicle
+  // charge state sensors
+  sensor::Sensor *batteryLevelSensor = nullptr;
+  text_sensor::TextSensor *chargingStateSensor = nullptr;
+  sensor::Sensor *chargerPowerSensor = nullptr;
+  sensor::Sensor *chargeRateSensor = nullptr;
+
+  // polling configuration
+  uint32_t charge_polling_interval_ = 10000; // 10 seconds default
+  uint32_t awake_polling_interval_ = 300000; // 5 minutes default
+  uint32_t last_charge_poll_ = 0;
+  uint32_t last_awake_poll_ = 0;
+  bool is_charging_ = false;
+  bool was_asleep_ =
+      true; // Track previous sleep state to detect wake transitions
+
+  std::vector<unsigned char> ble_read_buffer_;
+
+  void initializeFlash();
+  void openNVSHandle();
+  void initializePrivateKey();
+  void loadSessionInfo();
+  void loadDomainSessionInfo(UniversalMessage_Domain domain);
+};
+
+} // namespace tesla_ble_vehicle
 } // namespace esphome
